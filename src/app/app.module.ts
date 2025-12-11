66f4270ce2a76ffd59803924b6b985965167be91
Tuan <huutuan.contact@gmail.com>
2025-12-11 15:22:12 +0700

    chiu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DropMenuComponent } from './drop-menu/drop-menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ThumnailComponent } from './thumnail/thumnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DropMenuComponent,
+    SidebarComponent,
+    ThumnailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/drop-menu/drop-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-menu',
+  templateUrl: './drop-menu.component.html',
+  styleUrls: ['./drop-menu.component.css'],
+})
+export class DropMenuComponent {}
